f0066b3b68f54cbf3d9f04b6a61a8529da5f7a55
Megan Miller <megmil@umich.edu>
2024-03-23 18:02:55 -0400

    wrote vectorcanbus files

--- src/canbus/vectorcanbus.cpp
+++ src/canbus/vectorcanbus.cpp
@@ -1,35 +1,72 @@
-#include "canbus/vectorcanbus.hpp"
+#include <QVariant>
+#include "vectorcanbus.hpp"
 
-void VectorCANBus::connect(QString canInterface) {
-
-    if (!QCanBus::instance()->plugins().contains(QStringLiteral("vectorcan"))) {
-        qDebug() << "[VectorCANBus] 'vectorcan' plugin not available!";
-        return;
-    } else {
-        qDebug() << "[VectorCANBus] 'vectorcan' plugin available";
-    }
-
-    QString errorString;
-    bool connectionStatus;
-
-    bus = QCanBus::instance()->createDevice(
-        QStringLiteral("vectorcan"), canInterface, &errorString);
-    if (!bus) {
-        qDebug() << "[VectorCANBus] error creating CAN device: " << errorString;
-        return;
-    } else {
-        qDebug() << "[VectorCANBus] connecting CAN interface: " << canInferface;
-        bus->connectDevice();
-        // TODO: connect device
-    }
-
-//    if (!connectionStatus) {
-//        qDebug() << "Connection Failed!";
-//        return;
-//    }
-
-//    m_setupDone = true;
-//    qDebug() << "can1 connect successful";
+VectorCANBus::~VectorCANBus() {
+    qDebug() << "[VectorCANBus] disconnecting and deleting bus";
+    if (device->state() == QCanBusDevice::ConnectedState)
+        device->disconnectDevice();
 }
 
+void VectorCANBus::connectToCAN(QString channel) {
+    // check that QCanBus provides the VectorCAN plugin
+    if (!QCanBus::instance()->plugins().contains(QStringLiteral("vectorcan"))) {
+        qDebug() << "[VectorCANBus] 'vectorcan' plugin unavailable";
+        return;
+    }
+    qDebug() << "[VectorCANBus] 'vectorcan' plugin available";
 
+    // initialize device
+    QString errorString;
+    device = QCanBus::instance()->createDevice(
+        QStringLiteral("vectorcan"), channel, &errorString);
+    if (!device) {
+        qDebug() << "[VectorCANBus] error creating CAN device: " << errorString;
+        return;
+    }
+
+    // establish connections
+    bool connectionStatus = false;
+    qDebug() << "[VectorCANBus] connecting CAN interface: " << channel;
+    connect(device, &QCanBusDevice::errorOccurred,
+            this, &VectorCANBus::processErrors);
+    connect(device, &QCanBusDevice::stateChanged,
+            this, &VectorCANBus::processStateChange);
+    connect(device, &QCanBusDevice::framesReceived,
+            this, &VectorCANBus::processReceivedFrames);
+    connectionStatus = device->connectDevice();
+    if (!connectionStatus) {
+        qDebug() << "[VectorCANBus] connection failed";
+        return;
+    }
+    qDebug() << "[VectorCANBus] connection succeeded";
+}
+
+void VectorCANBus::processStateChange() {
+    qDebug() << "[VectorCANBus] QCanBusDevice::stateChanged: " << device->state();
+}
+
+void VectorCANBus::processErrors() {
+    qDebug() << "[VectorCANBus] QCanBusDevice::errorOccurred: " << device->errorString();
+}
+
+void VectorCANBus::processReceivedFrames() {
+    qDebug() << "[VectorCANBus] processing " << device->framesAvailable() << " frames";
+    QList<QCanBusFrame> frames = device->readAllFrames();
+    for (auto frame : frames) {
+        if (callbacks.find(frame.frameId()) == callbacks.end())
+            continue;
+        for (auto callback : callbacks[frame.frameId()])
+            callback(frame.payload());
+    }
+}
+
+void VectorCANBus::registerFrameHandler(int id, std::function<void(QByteArray)> callback) {
+    callbacks[id].push_back(callback);
+    QCanBusDevice::Filter filter;
+    filter.frameId = id;
+    filter.frameIdMask = 0xFFF;
+    filter.format=QCanBusDevice::Filter::MatchBaseAndExtendedFormat;
+    filter.type = QCanBusFrame::InvalidFrame;
+    filterList.append(filter);
+    device->setConfigurationParameter(QCanBusDevice::RawFilterKey, QVariant::fromValue(filterList));
+}
--- src/canbus/vectorcanbus.hpp
+++ src/canbus/vectorcanbus.hpp
@@ -3,28 +3,42 @@
 
 #include <QObject>
 #include <QCanBus>
-#include <QVector>
 #include <QtDebug>
-#include <QTimer>
-#include <QMap>
+#include <QCanBusDevice>
+#include <QByteArray>
+#include <QString>
+#include <QVector>
+#include <unordered_map>
+#include <vector>
 
 class VectorCANBus : public QObject {
     Q_OBJECT
-
-    QCanBusDevice* bus;
 
 public:
     explicit VectorCANBus(QObject *parent = nullptr);
     ~VectorCANBus();
 
-signals:
-    void onVectorDataChanged();
+private:
+    QCanBusDevice* device;
+    std::unordered_map<int, std::vector<std::function<void(QByteArray)>>> callbacks;
+    QVector<QCanBusDevice::Filter> filterList;
+
+    void processReceivedFrames();
+    void processErrors();
+    void processStateChange();
 
 public slots:
-    void connect();
+    /*
+    The VectorCAN plugin provides 64 channels (defined by XL_CONFIG_MAX_CHANNELS in the
+    Vector API) from can0 to can63. Some of these channels can be virtual, and therefore
+    can be used without actual CAN hardware. To find out the virtual channels, the
+    program "Vector Hardware Config" (vcanconf.exe) can be used, which is included in
+    Vector's driver package. The availableDevices() method returns a list of currently
+    available devices.
+    */
+    void connectToCAN(QString channel);
 
-private:
-    void setupFilters();
+    void registerFrameHandler(int id, std::function<void(QByteArray)> callback);
 
 };
 
